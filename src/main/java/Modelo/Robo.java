566ed4ce3e473ae5e88ff82b6ad18ffdb371801b
LucasNeves159 <70297301+LucasNeves159@users.noreply.github.com>
2021-06-21 10:45:05 -0300

    I like'em vulnerable ( ͡° ͜ʖ ͡°)
    (sorry, that's sleep deprivation)

--- src/main/java/Modelo/Robo.java
+++ src/main/java/Modelo/Robo.java
@@ -4,8 +4,11 @@ import java.util.Random;
 
 import Auxiliar.Desenhador;
 import Auxiliar.Posicao;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Robo extends Elemento {
+    private Timer timer;
     // Construtor de robo que recebe um nome de imagem e uma posicao inicial
     // Mata o heroi caso se choque com ele
     public Robo(String sNomeImagePNG, Posicao umaPosicao) {
@@ -16,6 +19,7 @@ public class Robo extends Elemento {
     }
 
     // Move-se randomicamente
+    @Override
     public void autoDesenho() {
         Random r = new Random();
         int iDirecao = r.nextInt(4);
@@ -43,5 +47,29 @@ public class Robo extends Elemento {
             this.getPosicao().volta();
         }
         super.autoDesenho();
+    }
+    
+    //Método que define estado de vulnerabilidade
+    public void setVulneravel(){
+        bTransponivel = !bTransponivel;
+        bMortal = !bMortal;
+    }
+    
+    public void ligaVulneravel(){
+        timer = new Timer();
+        TimerTask desliga;
+        
+        //definindo a task de desligar!
+        desliga = new TimerTask() {
+            public void run() {
+                setVulneravel();
+                System.out.println("Perigo!");
+            }
+        };
+        
+        //esse roda primeiro
+        this.setVulneravel();
+        //"temporizador agendado para rodar 'desliga' em 6 segundos"
+        timer.schedule(desliga, 6000);
     }
 }
